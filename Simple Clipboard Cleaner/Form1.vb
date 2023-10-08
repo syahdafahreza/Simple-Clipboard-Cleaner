8b63e970b0768c7895eec36765d791706a1ae360
Syahda Fahreza <syahdafahreza@gmail.com>
2023-10-08 20:33:21 +0700

    Initial Commit...

---
+++ Simple Clipboard Cleaner/Form1.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Windows.Forms
+Public Class Form1
+    Private Sub btncleanclip_Click(sender As Object, e As EventArgs) Handles btncleanclip.Click
+        ' Clear the clipboard contents
+        Clipboard.Clear()
+
+        ' Optionally, provide feedback to the user
+        MessageBox.Show("Clipboard cleared!", "Clipboard Cleared", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+End Class
